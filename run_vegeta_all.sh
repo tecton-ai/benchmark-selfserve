062cbc6703a61f7ab2c215496762383eb3ab40fa
yanhuang <yan@tecton.ai>
2025-01-31 11:00:14 -0800

    just works 1.0

--- run_vegeta_all.sh
+++ run_vegeta_all.sh
@@ -4,11 +4,15 @@
 # Use this as a "workspace" to loadtest multiple services as the same time,
 # or just as a reference to paste line(s) into your console.
 #
-
-./run_vegeta.py --service fs_mixed_5_feature_views --file -r 5 -d 10 -t 5000 &
-./run_vegeta.py --service fs_mixed_10_feature_views --file -r 5 -d 10 -t 5000 &
-./run_vegeta.py --service fs_mixed_18_feature_views --file -r 5 -d 10 -t 5000 &
-./run_vegeta.py --service fs_non_aggregate_1_feature_views --file -r 5 -d 10 -t 5000 &
-./run_vegeta.py --service fs_non_aggregate_2_feature_views --file -r 5 -d 10 -t 5000 &
-./run_vegeta.py --service fs_non_aggregate_4_feature_views --file -r 5 -d 10 -t 5000 &
+for i in $(seq 1 1000);
+do
+    ./run_vegeta.py --service fs_mixed_5_feature_views --file -r 1 -d 6000 -t 5000 &
+    ./run_vegeta.py --service fs_mixed_10_feature_views --file -r 1 -d 6000 -t 5000 &
+    ./run_vegeta.py --service fs_mixed_18_feature_views --file -r 1 -d 6000 -t 5000 &
+    ./run_vegeta.py --service fs_non_aggregate_1_feature_views --file -r 1 -d 6000 -t 5000 &
+    ./run_vegeta.py --service fs_non_aggregate_2_feature_views --file -r 1 -d 6000 -t 5000 &
+    ./run_vegeta.py --service fs_non_aggregate_4_feature_views --file -r 1 -d 6000 -t 5000 &
+    echo "$i * 6 QPS" &
+    sleep 15
+done
 
